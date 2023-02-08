77363d304adc7e6babbd01ac863395560fdb2f93
Edgars <Edgars.Kaufmanis@nology.io>
2023-02-08 16:04:48 +0000

    added nav bar

--- fullstack-frontend/src/App.jsx
+++ fullstack-frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
 
 const App = () => {
-  return <p>hello there</p>;
+  return (
+    <Router>
+      <Nav />
+    </Router>
+  );
 };
 
 export default App;
---
+++ fullstack-frontend/src/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import "./Nav.scss";
+import { Link } from "react-router-dom";
+import locationIcon from "../../assets/images/icon-location.png";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <img className="nav__icon" src={locationIcon} alt="location icon" />
+      <Link className="nav__item" to="/locations">
+        Places
+      </Link>
+
+      <Link className="nav__item" to="/location/new">
+        New
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
